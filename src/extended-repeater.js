b2bdc00cc75c0f34403e2cd0e00c5b344656017a
Vadim Sedelkin <vadim@sedelkin.ru>
2022-02-23 23:10:07 +0300

    feat: implement function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let newStr = "";
+  let localOptions = {
+    repeatTimes: (typeof options.repeatTimes !== "undefined") ? options.repeatTimes : 1,
+    separator: (typeof options.separator !== "undefined") ? options.separator : '+',
+    addition: (typeof options.addition !== "undefined") ? options.addition : '',
+    additionRepeatTimes: (typeof options.additionRepeatTimes !== "undefined") ? options.additionRepeatTimes : 1,
+    additionSeparator: (typeof options.additionSeparator !== "undefined") ? options.additionSeparator : '|',
+  };
+  
+  for(let i = 0; i < localOptions.repeatTimes; i++){
+    if(i !== 0)
+      newStr += "" + localOptions.separator + str;
+    else
+      newStr += str;
+    for(let j = 0; j < localOptions.additionRepeatTimes; j++){
+      if(j === 0)
+        newStr += "" + localOptions.addition;
+      else
+        newStr += "" + localOptions.additionSeparator + localOptions.addition;
+    }
+  }
+  return newStr;
 }
 
 module.exports = {
