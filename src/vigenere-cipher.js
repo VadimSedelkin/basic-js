506101d8ec80d1ef67512bd004c47c87b8f6fb89
Vadim Sedelkin <vadim@sedelkin.ru>
2022-02-24 00:41:29 +0300

    feat: implement class VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect){
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.alphabet = this.alphabet.split("");
+    this.mode = (isDirect === true || typeof isDirect === "undefined") ? 'direct' : 'reverse';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if(typeof message === "undefined" || typeof key === "undefined")
+      throw new Error('Incorrect arguments!');
+    let keyLength = key.length;
+    let currentKeyLetter = 0;
+    let encryptedMessage = "";
+    let encryptLetterPosition;
+    for(let i = 0; i < message.length; i++){
+      if(this.alphabet.indexOf(message[i].toUpperCase()) === -1){
+        encryptedMessage += message[i];
+        continue;
+      } 
+      encryptLetterPosition = (this.alphabet.indexOf(message[i].toUpperCase()) + this.alphabet.indexOf(key[currentKeyLetter++ % keyLength].toUpperCase())) % this.alphabet.length;
+      encryptedMessage += this.alphabet[encryptLetterPosition];
+    }
+    return (this.mode === "reverse") ? encryptedMessage.split("").reverse().join("") : encryptedMessage;
+  }
+
+  decrypt(encryptedMessage, key) {
+    if(typeof encryptedMessage === "undefined" || typeof key === "undefined")
+      throw new Error('Incorrect arguments!');
+    let keyLength = key.length;
+    let currentKeyLetter = 0;
+    let decryptedMessage = "";
+    let decryptLetterPosition;
+    for(let i = 0; i < encryptedMessage.length; i++){
+      if(this.alphabet.indexOf(encryptedMessage[i].toUpperCase()) === -1){
+        decryptedMessage += encryptedMessage[i];
+        continue;
+      } 
+      decryptLetterPosition = (this.alphabet.indexOf(encryptedMessage[i].toUpperCase()) - this.alphabet.indexOf(key[currentKeyLetter++ % keyLength].toUpperCase())) % this.alphabet.length;
+      if(decryptLetterPosition < 0)
+        decryptedMessage += this.alphabet[this.alphabet.length+decryptLetterPosition];
+      else
+        decryptedMessage += this.alphabet[decryptLetterPosition];
+    }
+    return (this.mode === "reverse") ? decryptedMessage.split("").reverse().join("") : decryptedMessage;
   }
 }
 
