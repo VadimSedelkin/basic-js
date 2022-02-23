048677c53b93f2b237a0c56dca10b2e612ba5f1f
Vadim Sedelkin <vadim@sedelkin.ru>
2022-02-23 20:45:55 +0300

    feat: implement chainMaker object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push("" + value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink( position ) {
+    if(position < 1 || position > this.getLength() || !Number.isInteger(position)){
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let stringChain = "";
+    for(let i = 0; i < this.getLength(); i++){
+      if(i === 0)
+        stringChain += `( ${this.chain[i]} )`;
+      else
+        stringChain += `~~( ${this.chain[i]} )`;
+    }
+    this.chain = [];
+    return stringChain;
   }
 };
 
